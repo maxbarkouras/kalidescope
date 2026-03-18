7483526dabe42bd6311579dacbcb417e739120e2
maxbarkouras <max@computer>
2026-03-18 14:02:12 -0700

    general tokenizer return

--- lexer.cpp
+++ lexer.cpp
@@ -1,5 +1,29 @@
-
+#include <string>
 
 enum Token{
     tok_eof = -1, tok_def = -2, tok_extern = -3, tok_identifier = -4, tok_number = -5
 };
+
+static std::string IdentifierStr;
+static double NumVal;
+
+static int gettok() {
+
+    static int LastChar = ' ';
+
+    while(isspace(LastChar))
+        LastChar = getchar();
+
+    if(isalpha(LastChar)){
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        if (IdentifierStr == "def")
+            return tok_def;
+        if (IdentifierStr == "extern")
+            return tok_extern;
+        return tok_identifier;
+    }
+
+}
