9bac517e5bf30fac359d294e4fc116150eb98e76
maxbarkouras <max@computer>
2026-03-18 14:11:44 -0700

    completed lexer

--- lexer.cpp
+++ lexer.cpp
@@ -1,7 +1,11 @@
 #include <string>
 
 enum Token{
-    tok_eof = -1, tok_def = -2, tok_extern = -3, tok_identifier = -4, tok_number = -5
+    tok_eof = -1,
+    tok_def = -2,
+    tok_extern = -3,
+    tok_identifier = -4,
+    tok_number = -5
 };
 
 static std::string IdentifierStr;
