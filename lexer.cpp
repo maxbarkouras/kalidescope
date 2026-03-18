d1ecc93f82409ddccc35a3b8ed2644a20349f5b0
maxbarkouras <max@computer>
2026-03-18 14:10:35 -0700

    finished tokenizer

--- lexer.cpp
+++ lexer.cpp
@@ -26,4 +26,31 @@ static int gettok() {
         return tok_identifier;
     }
 
+    if(isdigit(LastChar) || LastChar == '.'){
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    if(LastChar == '#'){
+        do {
+            LastChar = getchar();
+        } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+        
+        if(LastChar != EOF)
+            return gettok();
+    }
+
+    if(LastChar == EOF)
+        return tok_eof;
+
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+
 }
